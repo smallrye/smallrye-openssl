77fc027478a23362e4530c6b7f4f32cc4b98a21a
George Gastaldi <gegastaldi@gmail.com>
2026-04-24 14:12:29 -0300

    Remove unnecessary maven-archiver directory from pre-release hook

--- .github/workflows/hooks/pre-perform-release.sh
+++ .github/workflows/hooks/pre-perform-release.sh
@@ -4,5 +4,4 @@ set -e
 mkdir -p target/native-jars
 gh run download "$GITHUB_RUN_ID" -n native-linux-x86_64 -D target/native-jars
 gh run download "$GITHUB_RUN_ID" -n native-linux-aarch_64 -D target/native-jars
-mkdir -p target/native-jars/maven-archiver
 ls -la target/native-jars/
